da3b29145f34a3747ea304c3c37f87dbb23bb2bf
salma <lulusalma847@gmail.com>
2024-11-24 22:39:40 +0700

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// efek parallax
-let text = document.getElementById('text');
-let leaf = document.getElementById('leaf');
-let hill1 = document.getElementById('hill1');
-let hill4 = document.getElementById('hill4');
-let hill5 = document.getElementById('hill5');
-let lightbox = document.getElementById('lightbox');
-let lightboxImg = document.getElementById('lightbox-img');
-let closeLightbox = document.getElementById('close-lightbox');
-
-window.addEventListener('scroll', ()=> {
-    let value = window.scrollY;
-
-    text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -1 + 'px';
-    leaf.style.left = value * 1 + 'px';
-    hill5.style.left = value * 1 + 'px';
-    hill4.style.top = value * 1 + 'px';
-    hill1.style.marginTop = value * 1.5 + 'px';
-});
 
 // alert
 function showAlert() {
